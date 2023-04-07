33063c1da4b605c6c7462fccdd2817b0b15829d1
Jacques Girardi Busnardo <jacques.busnardo@gmail.com>
2023-04-06 21:38:58 -0300

    Create playing-mario-bros-theme.c

---
+++ playing-mario-bros-theme.c
@@ -0,0 +1,360 @@
+int pinoBuzzer = 6;
+ 
+void setup() {
+   // configura pino do buzzer como saída
+  pinMode(pinoBuzzer,OUTPUT);
+}
+ 
+void loop() {
+  tone(pinoBuzzer,660,100);
+  delay(150);
+  tone(pinoBuzzer,660,100);
+  delay(300);
+  tone(pinoBuzzer,660,100);
+  delay(300);
+  tone(pinoBuzzer,510,100);
+  delay(100);
+  tone(pinoBuzzer,660,100);
+  delay(300);
+  tone(pinoBuzzer,770,100);
+  delay(550);
+  tone(pinoBuzzer,380,100);
+  delay(575);
+
+  tone(pinoBuzzer,510,100);
+  delay(450);
+  tone(pinoBuzzer,380,100);
+  delay(400);
+  tone(pinoBuzzer,320,100);
+  delay(500);
+  tone(pinoBuzzer,440,100);
+  delay(300);
+  tone(pinoBuzzer,480,80);
+  delay(330);
+  tone(pinoBuzzer,450,100);
+  delay(150);
+  tone(pinoBuzzer,430,100);
+  delay(300);
+  tone(pinoBuzzer,380,100);
+  delay(200);
+  tone(pinoBuzzer,660,80);
+  delay(200);
+  tone(pinoBuzzer,760,50);
+  delay(150);
+  tone(pinoBuzzer,860,100);
+  delay(300);
+  tone(pinoBuzzer,700,80);
+  delay(150);
+  tone(pinoBuzzer,760,50);
+  delay(350);
+  tone(pinoBuzzer,660,80);
+  delay(300);
+  tone(pinoBuzzer,520,80);
+  delay(150);
+  tone(pinoBuzzer,580,80);
+  delay(150);
+  tone(pinoBuzzer,480,80);
+  delay(500);
+
+  tone(pinoBuzzer,510,100);
+  delay(450);
+  tone(pinoBuzzer,380,100);
+  delay(400);
+  tone(pinoBuzzer,320,100);
+  delay(500);
+  tone(pinoBuzzer,440,100);
+  delay(300);
+  tone(pinoBuzzer,480,80);
+  delay(330);
+  tone(pinoBuzzer,450,100);
+  delay(150);
+  tone(pinoBuzzer,430,100);
+  delay(300);
+  tone(pinoBuzzer,380,100);
+  delay(200);
+  tone(pinoBuzzer,660,80);
+  delay(200);
+  tone(pinoBuzzer,760,50);
+  delay(150);
+  tone(pinoBuzzer,860,100);
+  delay(300);
+  tone(pinoBuzzer,700,80);
+  delay(150);
+  tone(pinoBuzzer,760,50);
+  delay(350);
+  tone(pinoBuzzer,660,80);
+  delay(300);
+  tone(pinoBuzzer,520,80);
+  delay(150);
+  tone(pinoBuzzer,580,80);
+  delay(150);
+  tone(pinoBuzzer,480,80);
+  delay(500);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,760,100);
+  delay(100);
+  tone(pinoBuzzer,720,100);
+  delay(150);
+  tone(pinoBuzzer,680,100);
+  delay(150);
+  tone(pinoBuzzer,620,150);
+  delay(300);
+
+  tone(pinoBuzzer,650,150);
+  delay(300);
+  tone(pinoBuzzer,380,100);
+  delay(150);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+  tone(pinoBuzzer,500,100);
+  delay(100);
+  tone(pinoBuzzer,570,100);
+  delay(220);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,760,100);
+  delay(100);
+  tone(pinoBuzzer,720,100);
+  delay(150);
+  tone(pinoBuzzer,680,100);
+  delay(150);
+  tone(pinoBuzzer,620,150);
+  delay(300);
+
+  tone(pinoBuzzer,650,200);
+  delay(300);
+
+  tone(pinoBuzzer,1020,80);
+  delay(300);
+  tone(pinoBuzzer,1020,80);
+  delay(150);
+  tone(pinoBuzzer,1020,80);
+  delay(300);
+
+  tone(pinoBuzzer,380,100);
+  delay(300);
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,760,100);
+  delay(100);
+  tone(pinoBuzzer,720,100);
+  delay(150);
+  tone(pinoBuzzer,680,100);
+  delay(150);
+  tone(pinoBuzzer,620,150);
+  delay(300);
+
+  tone(pinoBuzzer,650,150);
+  delay(300);
+  tone(pinoBuzzer,380,100);
+  delay(150);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+  tone(pinoBuzzer,500,100);
+  delay(100);
+  tone(pinoBuzzer,570,100);
+  delay(420);
+
+  tone(pinoBuzzer,585,100);
+  delay(450);
+
+  tone(pinoBuzzer,550,100);
+  delay(420);
+
+  tone(pinoBuzzer,500,100);
+  delay(360);
+
+  tone(pinoBuzzer,380,100);
+  delay(300);
+  tone(pinoBuzzer,500,100);
+  delay(300);
+  tone(pinoBuzzer,500,100);
+  delay(150);
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,760,100);
+  delay(100);
+  tone(pinoBuzzer,720,100);
+  delay(150);
+  tone(pinoBuzzer,680,100);
+  delay(150);
+  tone(pinoBuzzer,620,150);
+  delay(300);
+
+  tone(pinoBuzzer,650,150);
+  delay(300);
+  tone(pinoBuzzer,380,100);
+  delay(150);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+  tone(pinoBuzzer,500,100);
+  delay(100);
+  tone(pinoBuzzer,570,100);
+  delay(220);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,760,100);
+  delay(100);
+  tone(pinoBuzzer,720,100);
+  delay(150);
+  tone(pinoBuzzer,680,100);
+  delay(150);
+  tone(pinoBuzzer,620,150);
+  delay(300);
+
+  tone(pinoBuzzer,650,200);
+  delay(300);
+
+  tone(pinoBuzzer,1020,80);
+  delay(300);
+  tone(pinoBuzzer,1020,80);
+  delay(150);
+  tone(pinoBuzzer,1020,80);
+  delay(300);
+
+  tone(pinoBuzzer,380,100);
+  delay(300);
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,760,100);
+  delay(100);
+  tone(pinoBuzzer,720,100);
+  delay(150);
+  tone(pinoBuzzer,680,100);
+  delay(150);
+  tone(pinoBuzzer,620,150);
+  delay(300);
+
+  tone(pinoBuzzer,650,150);
+  delay(300);
+  tone(pinoBuzzer,380,100);
+  delay(150);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+
+  tone(pinoBuzzer,500,100);
+  delay(300);
+  tone(pinoBuzzer,430,100);
+  delay(150);
+  tone(pinoBuzzer,500,100);
+  delay(100);
+  tone(pinoBuzzer,570,100);
+  delay(420);
+
+  tone(pinoBuzzer,585,100);
+  delay(450);
+
+  tone(pinoBuzzer,550,100);
+  delay(420);
+
+  tone(pinoBuzzer,500,100);
+  delay(360);
+
+  tone(pinoBuzzer,380,100);
+  delay(300);
+  tone(pinoBuzzer,500,100);
+  delay(300);
+  tone(pinoBuzzer,500,100);
+  delay(150);
+  tone(pinoBuzzer,500,100);
+  delay(300);
+
+  tone(pinoBuzzer,500,60);
+  delay(150);
+  tone(pinoBuzzer,500,80);
+  delay(300);
+  tone(pinoBuzzer,500,60);
+  delay(350);
+  tone(pinoBuzzer,500,80);
+  delay(150);
+  tone(pinoBuzzer,580,80);
+  delay(350);
+  tone(pinoBuzzer,660,80);
+  delay(150);
+  tone(pinoBuzzer,500,80);
+  delay(300);
+  tone(pinoBuzzer,430,80);
+  delay(150);
+  tone(pinoBuzzer,380,80);
+  delay(600);
+
+  tone(pinoBuzzer,500,60);
+  delay(150);
+  tone(pinoBuzzer,500,80);
+  delay(300);
+  tone(pinoBuzzer,500,60);
+  delay(350);
+  tone(pinoBuzzer,500,80);
+  delay(150);
+  tone(pinoBuzzer,580,80);
+  delay(150);
+  tone(pinoBuzzer,660,80);
+  delay(550);
+
+  tone(pinoBuzzer,870,80);
+  delay(325);
+  tone(pinoBuzzer,760,80);
+  delay(600);
+
+  tone(pinoBuzzer,500,60);
+  delay(150);
+  tone(pinoBuzzer,500,80);
+  delay(300);
+  tone(pinoBuzzer,500,60);
+  delay(350);
+  tone(pinoBuzzer,500,80);
+  delay(150);
+  tone(pinoBuzzer,580,80);
+  delay(350);
+  tone(pinoBuzzer,660,80);
+  delay(150);
+  tone(pinoBuzzer,500,80);
+  delay(300);
+  tone(pinoBuzzer,430,80);
+  delay(150);
+  tone(pinoBuzzer,380,80);
+  delay(600);
+
+  tone(pinoBuzzer,660,100);
+  delay(150);
+  tone(pinoBuzzer,660,100);
+  delay(300);
+  tone(pinoBuzzer,660,100);
+  delay(300);
+  tone(pinoBuzzer,510,100);
+  delay(100);
+  tone(pinoBuzzer,660,100);
+  delay(300);
+  tone(pinoBuzzer,770,100);
+  delay(550);
+  tone(pinoBuzzer,380,100);
+  delay(575);
+}
